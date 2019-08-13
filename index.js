dbba707878d81e52d9af0f500c9527bfcd91c0c1
Jackson Mooring <Gninoskcaj@Elijahs-Mac-mini.local>
2019-08-13 13:27:52 -0500

    Finish

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { execSync } = require('child_process');
 const fs = require('fs')
+const { spawnSync } = require('child_process')
 
 
-execSync('git clone https://github.com/svelte-toolbox/website')
+execSync('git clone https://github.com/svelte-toolbox/website EX_FOLDER_000_111_222_333')
 console.log("Cloned")
-fs.copyFileSync('website/.gitignore', '.easycheese')
-fs.unlinkSync('website')
+spawnSync('mv', ['EX_FOLDER_000_111_222_333/__sapper__/build*', '/'])
+execSync('rm -Rf EX_FOLDER_000_111_222_333')
