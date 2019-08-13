67603e5cb2c9c2014f2476a8ee7c5a19cea68765
Jackson Mooring <Gninoskcaj@Elijahs-Mac-mini.local>
2019-08-13 13:47:11 -0500

    FIx a bug

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const { spawnSync } = require('child_process')
 
 execSync('git clone https://github.com/svelte-toolbox/website EX_FOLDER_000_111_222_333')
 console.log("Cloned")
-spawnSync('mv', ['EX_FOLDER_000_111_222_333/__sapper__/build*', '/'])
+spawnSync('mv', ['EX_FOLDER_000_111_222_333/__sapper__/build/*', '/'])
 execSync('rm -Rf EX_FOLDER_000_111_222_333')
